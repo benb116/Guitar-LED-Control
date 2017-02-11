6fa4c186ccbb02fd365e67a46227dae68f3c0ba0
benb116 <benb116@gmail.com>
2017-02-10 21:40:02 -0500

    Getting there

--- src/main.c
+++ src/main.c
@@ -3,21 +3,39 @@
 #include "m_rf.h"
 #include "m_usb.h"
 
-#define RES 4000
-#define NUMVAL 600
+#define RES 5000
+#define NUMPERS 30
+#define PERDIFF 10
+#define FREQTOL 0.05
 
 volatile int ledstatus[9] = {0};
-int vals[NUMVAL] = {0};
+int pers[NUMPERS] = {0};
+int persShift[NUMPERS];
+int pers2nd[NUMPERS/2];
+int pers3rd[NUMPERS/3];
+
 
 int enable = 1;
 int h = 0;
+
 int i = 0;
-int curval = 0;
+bool ishi = 0;
+int perInd = 0;
+
 int dripIndex = 0;
 
 void initTimer1();
-void initADC();
 void initPins();
+
+void tuneCheck();
+void recordPer(int tick);
+float calcFreq();
+int calcDiff(float freq, float tru);
+void tuneLED();
+
+int min_ind(int array[], int size);
+int max_ind(int array[], int size);
+int avg(int array[], int size);
 
 void allOn();
 
@@ -33,34 +51,50 @@ int main(void)
 	m_bus_init();
 	m_usb_init();
 	while(!m_usb_isconnected()) {}
+
 	initTimer1();
 	initPins();
-	initADC();
+
 	sei(); // Enable interrupts
 	m_red(ON);
+
 	while(1) {
-		if (curval > 100) {
-			// m_green(ON);
-			ledstatus[1] = 1;
-			// set(PORTB, 3);
-		} else {
-			// m_green(OFF);
-			ledstatus[1] = 0;
-			// clear(PORTB, 3);
-		}
 		// allOn();
 		// dripLED();
+		tuneLED();
 		lightLEDs();
+		m_usb_tx_int(perInd);
+		m_usb_tx_string("\n");
 	}
 	return 0;
 }
 
 ISR(TIMER1_OVF_vect) { // When OCR1A is reached (4 kHz)
-	dripIncrement();
+	// dripIncrement();
+	tuneCheck();
+}
 
-	curval = ADC;
-	vals[i] = curval;
-	
+void tuneCheck() {
+	if (check(PINB, 4)) {
+		if (ishi) {
+			++i;
+		} else {
+			recordPer(i);
+			i = 0;
+			ishi = true;
+		}
+	} else {
+		if (ishi) {
+			ishi = false;
+		}
+		++i;
+	}
+}
+
+void recordPer(int tick) {
+	++perInd;
+	if (perInd >= NUMPERS) {perInd = 0;}
+	pers[perInd] = tick;
 }
 
 void initTimer1() {
@@ -78,29 +112,6 @@ void initTimer1() {
 	OCR1A = count;
 }
 
-void initADC() {
-	clear(ADMUX, REFS1); // Set ref voltage to VCC
-	set(ADMUX, REFS0);
-
-	set(ADCSRA, ADPS1); // Set clock scale CHANGE IF clockdivide CHANGES!!!!!!
-	set(ADCSRA, ADPS0); // Set clock scale CHANGE IF clockdivide CHANGES!!!!!!
-
-    set(DIDR2, ADC11D); // Set as analog input B4
-
-	set(ADCSRA, ADIE); // Enable interrupting
-
-	set(ADCSRA, ADATE); // Set triggering
-
-	set(ADCSRB, MUX5); // Look at B4
-	clear(ADMUX, MUX2);
-	set(ADMUX, MUX1);
-	set(ADMUX, MUX0);
-
-	set(ADCSRA, ADEN); // Conversion
-	set(ADCSRA, ADSC);
-
-	set(ADCSRA, ADIF); // Reading result
-}
 void initPins() {
 	set(DDRB, 2);
 	set(DDRB, 3);
@@ -111,7 +122,7 @@ void initPins() {
 	set(DDRD, 6);
 	set(DDRF, 0);
 	set(DDRF, 1);
-	// clear(DDRB, 4); // B4 is ADC Input
+	clear(DDRB, 4); // B4 is input
 
 	clear(PORTB, 2);
 	clear(PORTB, 3);
@@ -122,7 +133,112 @@ void initPins() {
 	clear(PORTD, 6);
 	clear(PORTF, 0);
 	clear(PORTF, 1);
-	// clear(PORTB, 4); // Disable pull-up resistor
+	clear(PORTB, 4); // Disable pull-up resistor
+}
+
+void tuneLED() {
+	float freq = calcFreq();
+	int LED;
+
+	if 		(freq < 70) 	{LED = 0;} 
+	else if (freq < 96.2) 	{LED = calcDiff(freq, 82.40);} 
+	else if (freq < 128.4) 	{LED = calcDiff(freq, 110.0);} 
+	else if (freq < 171.4) 	{LED = calcDiff(freq, 146.8);} 
+	else if (freq < 221.4) 	{LED = calcDiff(freq, 196.0);} 
+	else if (freq < 288.3) 	{LED = calcDiff(freq, 246.9);} 
+	else if (freq < 350) 	{LED = calcDiff(freq, 329.6);} 
+	else 					{LED = 0;}
+
+	int j;
+	for (j = 0; j < 9; ++j) {
+		ledstatus[j] = (j == (LED - 1));
+	}
+}
+
+float calcFreq() {
+	int j;
+	int k = (perInd+1);
+	for (j = k; j < NUMPERS; ++j) {
+		persShift[(j-k)] = pers[j];
+	}
+	for (j = 0; j < k; ++j) {
+		persShift[(j+k)] = pers[j];
+	}
+	int maxPer = persShift[max_ind(persShift, NUMPERS)];
+	int minPer = persShift[min_ind(persShift, NUMPERS)];
+
+	if (maxPer - minPer < PERDIFF) {
+
+	}
+
+
+	for (j = 0; j < (NUMPERS/2); ++j) {
+		pers2nd[j] = persShift[(2*j)] + persShift[(2*j-1)] ;
+	}
+	for (j = 0; j < (NUMPERS/3); ++j) {
+		pers3rd[j] = persShift[(3*j)] + persShift[(3*j-1)] + persShift[(3*j-2)];
+	}
+}
+
+// Find the index of the minimum value of an array
+int min_ind(int array[], int size) {
+	int min = array[0];
+	int ind = 0;
+	int j;
+	for (j = 1; j < size; ++j) {
+		if (array[j] < min) {
+			min = array[j];
+			ind = j;
+		}
+	}
+	return ind;
+}
+
+// Find the index of the maximum value of an array
+int max_ind(int array[], int size) {
+	int max = array[0];
+	int ind = 0;
+	int j;
+	for (j = 1; j < size; ++j) {
+		if (array[j] > max) {
+			max = array[j];
+			ind = j;
+		}
+	}
+	return ind;
+}
+
+int avg(int array[], int size) {
+	int sum = 0;
+	int j;
+	for (j = 0; j < size; ++j) {
+		sum += array[j];
+	}
+	int avgval = sum / size;
+	return avgval;
+}
+
+int calcDiff(float freq, float tru) {
+	float notehi = tru * 1.059463094;
+	float notelo = tru / 1.059463094;
+	float frac;
+	if (freq > tru) {
+		frac = (freq - tru) / (notehi - tru);
+	} else {
+		frac = (freq - tru) / (tru - notelo);
+	}
+	float LEDind = 5 + (frac / FREQTOL);
+	if (LEDind < 1) {LEDind = 1;}
+	if (LEDind > 9) {LEDind = 9;}
+	int LE = LEDind + 0.5;
+	// if(usbDebug) {
+	// 	m_usb_tx_string(" Compto = ");
+	// 	m_usb_tx_int(tru * 10);
+	// 	m_usb_tx_string(" LED = ");
+	// 	m_usb_tx_int(LE);
+	// }
+
+	return LE;
 }
 
 void dripIncrement() {
@@ -136,11 +252,6 @@ void dripIncrement() {
 		dripIndex = 0;
 	}
 	++h;
-
-	++i;
-	if (i >= NUMVAL) {
-		i = 0;
-	}
 }
 
 void dripLED() {
